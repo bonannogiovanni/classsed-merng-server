ed47390685723fddaf85394478c5d6b77274b1d1
Giovanni Bonanno <bonannogiovanni@hotmail.com>
2021-03-30 22:28:03 +0200

    config not ignored by git

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://classsed:CD6qR9ib3Bl6xGrH@cluster0.b4fbw.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
